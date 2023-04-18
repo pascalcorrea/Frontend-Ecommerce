eb581e3d025891aafe770626b0aad5579c72f165
pascalcorrea <98128852+pascalcorrea@users.noreply.github.com>
2023-04-17 21:28:09 -0400

    3

--- src/Config/axiosClient.js
+++ src/Config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:4000"
+    baseURL: "https://vapzstore.onrender.com"
 })
 
 export default axiosClient;
